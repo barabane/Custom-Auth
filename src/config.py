fcaafa809c09a28e1261dc5183e9e8bbac3a1447
Vladislav <barabayne@gmail.com>
2024-10-10 23:40:41 +0500

    feat: add db connection and alembic

--- src/config.py
+++ src/config.py
@@ -1,8 +1,33 @@
+from typing import Literal
+
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+class CustomBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+class Settings(CustomBaseSettings):
+    MODE: Literal['TEST', 'PROD', 'DEV'] = 'DEV'
+
+    POSTGRES_NAME: str
+    POSTGRES_PASS: str
+    POSTGRES_USER: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+
+    DATABASE_URL: PostgresDsn | None
+
+    def __init__(self):
+        self.DATABASE_URL = PostgresDsn.build(
+            scheme='postgresql+asyncpg',
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASS,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_NAME,
+        )
 
 
 settings = Settings()
--- src/database/database.py
+++ src/database/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
+from src.config import settings
+
 Base = DeclarativeBase()
 
-engine = create_async_engine()
+engine = create_async_engine(url=settings.DATABASE_URL)
 async_session_maker = sessionmaker(
     bind=engine, class_=AsyncSession, autoflush=False, expire_on_commit=False
 )
